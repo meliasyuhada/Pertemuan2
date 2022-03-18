659d7fdafa37f9dd35f9487151b4582ab598838c
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-18 10:47:57 +0700

    Contoh collections object set

---
+++ src/main/kotlin/Set.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //Set<T> stares unique elements; their order is generally underfined
+    val numbers = setOf(1, 2, 3, 4)
+    println("Number of elements: ${numbers.size}")
+    if (numbers.contains(1))
+        println("1 is in the set")
+
+    val numbersBackwards = setOf(4, 3, 2, 1)
+    println("The sets are equal: ${numbers == numbersBackwards}")
+
+    //the default implementation of set - LinkedHashSet - preserves the order of elents insertion
+    println(numbers.first() == numbersBackwards.first())
+    println(numbers.first() == numbersBackwards.last())
+
+    val strings = hashSetOf("a", "b", "c", "c") //hash set value unik
+    println("My Set Values are " +strings)
+}
