6167a63fed6245c0fb9e04382196005c80d9c6a7
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-24 22:07:54 +0700

    Contoh Fungsi Ekspresi Tunggal

---
+++ src/main/kotlin/FungsiEkspresiTunggal.kt
@@ -0,0 +1,17 @@
+import kotlin.math.max
+
+//cara menulis kode program fungsi yang ringkas
+
+fun main() {
+    println("Max value from max function = ${max(4, 7)}")
+    println("Max value from newMax function = ${newMax(3, 5)}")
+}
+
+fun max(a: Int, b:Int): Int {
+    val  maxValue = if (a > b)
+        a else b
+    return maxValue
+}
+
+fun newMax(a: Int, b:Int): Int = if (a > b)
+    a else b
