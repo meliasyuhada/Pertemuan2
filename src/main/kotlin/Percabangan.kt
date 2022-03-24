41282d2d1922372a5ad0a8708936b0ae910a1710
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-24 21:23:27 +0700

    Contoh Percabangan 1

---
+++ src/main/kotlin/Percabangan.kt
@@ -0,0 +1,37 @@
+//IF, IF-ELSE, IF-ELSE IF
+
+fun main() {
+    //londitions
+    if (15 > 5)
+        println("True")
+    println("The program continues here ...")
+
+    //if-else
+    val a: Int = 5
+    val b: Int = 2
+    var max: Int
+
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+    println("Maximum of a or b is " + max)
+
+    //if-else if
+    var myVar: Int = 0
+    if (myVar == 0) {
+        myVar = 1
+    } else if (myVar == 1) {
+        myVar = 0
+    } else {
+        myVar = -1
+    }
+    println("Nilai myVar adalah " + myVar)
+
+    print("\nEnter a number : ")
+    val intNumber = readLine()!!.toInt()
+    if (intNumber > 5)
+        println("The number you entered is greater than 5!")
+    println("Thanks for the input!")
+}
